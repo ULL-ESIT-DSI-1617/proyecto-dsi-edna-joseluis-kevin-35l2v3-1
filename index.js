b4b78f8ee74889c654afdcacd761924fb38a2f7f
alu0100821390 <alu0100821390@ull.edu.es>
2017-05-18 19:00:02 +0100

    Añadir diseño web inicial

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+// var bodyParser = require('body-parser');
+var path = require('path');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/.'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var signin = require('./routes/signin');
+app.use('/signin', signin);
+
+var signout = require('./routes/signout');
+app.use('/signout', signout);
+
+var jscalculator = require('./routes/jscalculator');
+app.use('/jscalculator', jscalculator);
+
+
+var server = app.listen(app.get('port'), function() {
+	console.log('Servidor escuchando en el puerto %s', app.get('port'));
+});
