7c7730d610cde93b376e4094150a5e3b607c5014
ednagc <ednagc.11@gmail.com>
2017-05-23 21:18:52 +0100

    Modificando signin

--- routes/signin.js
+++ routes/signin.js
@@ -3,12 +3,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+var configDB = require('../config/database');
+mongoose.connect(configDB.url);
+var User = require('../models/user');
+
 router.get('/', function(req, res) {
 	res.render('signin');
 });
 
 router.post('/', function(req, res) {
 	// Comprobar inicio de sesión de usuario registrado...
+	if(!req.body.username || !req.body.passwd){
+
+		console.log('No has introducido tus datos!');
+
+	}else{
+							//Buscamos en la base de datos
+				});
+			}
+		});
+	}
+
+
 });
 
 module.exports = router;
