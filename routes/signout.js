521c5964df5de1211cf5d8aafb75d21bab6f1908
opelu <opelu8@gmail.com>
2017-05-23 20:16:39 +0000

    Creando conexion a Mongo

--- routes/signout.js
+++ routes/signout.js
@@ -2,6 +2,12 @@
 
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var configDB = require('../config/database');
+mongoose.connect(configDB.url);
+var User = require('../models/user');
+
 
 router.get('/', function(req, res) {
 	res.render('signout');
