5a45ffe397220b39c15ed3cb16a8e7036054cfba
ednagc <ednagc.11@gmail.com>
2017-05-23 22:05:28 +0100

    Corrigiendo

--- routes/signin.js
+++ routes/signin.js
@@ -2,12 +2,12 @@
 
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 
 var configDB = require('../config/database');
 mongoose.connect(configDB.url);
 var User = require('../models/user');
-var mongoose = require('mongoose');
 
 router.get('/', function(req, res) {
 	res.render('signin');
