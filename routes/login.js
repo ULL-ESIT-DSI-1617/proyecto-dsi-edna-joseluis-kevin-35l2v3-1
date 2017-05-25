88ffd9ae098bea3dc11bc9a931d141771b887b79
Lily Galiano <lilygaliano@MacBook-Air-de-Lily.local>
2017-05-25 13:03:56 +0100

    Hay error

--- routes/login.js
+++ routes/login.js
@@ -20,16 +20,23 @@ router.post('/', function(req, res) {
 		console.log('No has introducido tus datos!');
 
 	}else{
-			User.findOne({username:req.body.username, password : req.body.passwd}, function (err, result){
+			User.findOne({username:req.body.username}, function (err, result){
 					if(result !== null){
-							console.log('Login correcto!');
-							res.render('jscalculator');
+
+							let rs = result.validPassword(req.body.passwd1,result.password);
+							console.log(ls);
+
+							if (result.username == req.body.username && rs == true){
+
+								// console.log('Login correcto!');
+								// res.render('jscalculator');
+							}
 						}else{
 							User.findOne({username:req.body.username}, function (err, result){
 									if(result == null){
-										
-									}else{
-										
+										console.log("Registrate primero para acceder");    
+										res.render('register', { header: "ERROR",
+							   			message: "ERROR usuario no registrado" });
 									}
 						});
 					}
